09693f01e86055ccb5a554dd5df71f9e10a05c2f
hanilcome <126670478+hanilcome@users.noreply.github.com>
2023-05-23 13:36:26 +0900

    [level 0] Title: 안전지대, Time: 0.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.py
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -1,20 +1,19 @@
 def solution(board):
     # 1을 먼저 찾는다.
-    # 인접한곳을 1로 바꾼다.
+    # 인접한곳을 2로 바꾼다.
     # 0의 개수를 센다.
     answer = len(board)*len(board)
     
     for x, row in enumerate(board):
         for y, length in enumerate(row):
             if board[x][y] == 1:
-                if board[x][y] == 1:
-                    answer -= 1
-                    for i in range(x-1, x+2):
-                        for j in range(y-1, y+2):
-                            if 0 <= i < len(board) and 0 <= j < len(board):
-                                if board[i][j] != 1 and board[i][j] != 2:
-                                    board[i][j] = 2
-                                    answer -= 1  
+                answer -= 1
+                for i in range(x-1, x+2):
+                    for j in range(y-1, y+2):
+                        if 0 <= i < len(board) and 0 <= j < len(board):
+                            if board[i][j] != 1 and board[i][j] != 2:
+                                board[i][j] = 2
+                                answer -= 1  
 
                 
     return answer

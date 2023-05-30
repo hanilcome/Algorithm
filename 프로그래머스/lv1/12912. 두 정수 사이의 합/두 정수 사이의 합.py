d62a1b2d30409e0138a8882ee71e1cf98726f978
hanilcome <126670478+hanilcome@users.noreply.github.com>
2023-05-31 01:06:05 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.14 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -2,9 +2,7 @@ def solution(a, b):
         sum = 0
         temp = 0
         if a > b:
-                temp = a
-                a = b
-                b = temp
+                a, b = b, a
         for s in range(a, b+1):
                 sum += s
         return sum

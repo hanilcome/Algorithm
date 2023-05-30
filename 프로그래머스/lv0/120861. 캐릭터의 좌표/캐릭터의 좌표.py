bfe45ea89e7f167cf9c6285689dad0b7ad626b70
hanilcome <126670478+hanilcome@users.noreply.github.com>
2023-05-30 23:39:25 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,14 @@
+def solution(keyinput, board):
+  answer = [0,0]
+  
+  for a in keyinput:
+    if a == "left" and answer[0]-1 >= -(board[0]//2):
+      answer[0] -= 1
+    elif a == "right" and answer[0]+1 <= board[0]//2:
+      answer[0] += 1
+    elif a == "up" and answer[1]+1 <= board[1]//2:
+      answer[1] += 1
+    elif a == "down" and answer[1]-1 >= -(board[1]//2):
+      answer[1] -= 1
+      
+  return answer

1e4a06186feb1852919909bcded26dc346fd5729
hanilcome <126670478+hanilcome@users.noreply.github.com>
2023-06-08 20:50:31 +0900

    [unrated] Title: 문자열 나누기, Time: 4.51 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
@@ -0,0 +1,24 @@
+def solution(s):
+    slist = []
+    for a in s:
+        slist.append(a)
+
+    answer = 1
+    same = 0
+    another = 0
+
+    for a, n in enumerate(slist):
+        if same == 0:
+            same += 1
+        elif same != another:
+            if slist[0] == slist[a]:
+                same += 1
+            elif slist[0] != slist[a]:
+                another += 1
+        else:   
+            answer += 1
+            same = 1
+            another = 0
+            slist[0] = n
+            
+    return answer
